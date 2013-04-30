9d38984c786e3fb05099ca6d8400075e27d1bb75
Matti Paksula <matti.paksula@iki.fi>
2013-04-30 12:25:20 +0300

    get users and messages

--- README.md
+++ README.md
@@ -12,7 +12,12 @@ Flowdock Digest has support for RVM (.ruby-version and .ruby-gemset).
 ## Installation in Heroku
 
     $ heroku create <name>
+
+    $ heroku config:set FLOWDOCK_DIGEST_PERSONAL_API_TOKEN=<API token of the user>
     $ heroku config:set FLOWDOCK_DIGEST_FLOW_API_TOKEN=<API token of the flow>
+    $ heroku config:set FLOWDOCK_DIGEST_ORGANIZATION=<name of your organization>
+    $ heroku config:set FLOWDOCK_DIGEST_FLOW=<name of your flow>
+
     $ git push heroku master
 
 
--- sender.rb
+++ sender.rb
@@ -1,5 +1,35 @@
 require "httparty"
 
-puts HTTParty.get('http://twitter.com/statuses/public_timeline.json')
+personal_api_token = ENV['FLOWDOCK_DIGEST_PERSONAL_API_TOKEN']
+flow_api_token = ENV['FLOWDOCK_DIGEST_FLOW_API_TOKEN']
+organization = ENV['FLOWDOCK_DIGEST_ORGANIZATION']
+flow_name = ENV['FLOWDOCK_DIGEST_FLOW']
 
-puts ENV['FLOWDOCK_DIGEST_FLOW_API_TOKEN']
+auth = {
+  :username => personal_api_token,
+  :password => flow_api_token
+}
+
+
+class Message
+
+  attr_accessor :attributes
+
+  def initialize(opts)
+    @attributes = opts
+  end
+end
+
+users = "https://api.flowdock.com/flows/#{organization}/#{flow_name}/users"
+messages = "https://api.flowdock.com/flows/#{organization}/#{flow_name}/messages?limit=1&event=message"
+
+response = HTTParty.get(users,
+  :basic_auth => auth)
+
+puts response.parsed_response.inspect
+
+response = HTTParty.get(messages,
+  :basic_auth => auth)
+
+puts response.parsed_response.inspect
+
